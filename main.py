a99dd9c766ddc786744483410c27bc91c0cea96a
Leopoldo Medri <medribros@gmail.com>
2024-01-15 09:07:59 -0300

    api connection

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
-
+import requests
 
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///movies-collection.db"
 db.init_app(app)
+
+
+TOKEN_ACCESS = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMDhhNGZiZjZiOTZlYjc5M2Q5MmE4ZWNlNTYzZGE5NSIsInN1YiI6IjY1YTUwNTQwMGYyYWUxMDEyZDViMzg1YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rIIztOdrOFL8pNag5NvlhNk5HFvAdoB9R44Z4jJVX84"
+API_KEY = "008a4fbf6b96eb793d92a8ece563da95"
+API_END_POINT = "https://api.themoviedb.org/3/search/movie"
+
+
 
 
 class Movies(db.Model):
@@ -24,29 +31,11 @@ class Movies(db.Model):
 with app.app_context():
     db.create_all()
 
-    new_movie = Movies(
-        title="Phone Booth",
-        year=2002,
-        description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-        rating=7.3,
-        ranking=10,
-        review="My favourite character was the caller.",
-        img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-    )
-    # db.session.add(new_movie)
-    # db.session.commit()
-    second_new_movie = Movies(
-        title="Avatar The Way of Water",
-        year=2022,
-        description="Set more than a decade after the events of the first film, learn the story of the Sully family (Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each other safe, the battles they fight to stay alive, and the tragedies they endure.",
-        rating=7.3,
-        ranking=9,
-        review="I liked the water.",
-        img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-    )
 
-    # db.session.add(second_new_movie)
-    # db.session.commit()
+
+
+
+
 @app.route("/")
 def index():
     all_movies = db.session.execute(db.select(Movies).order_by(Movies.rating)).scalars()
@@ -76,6 +65,58 @@ def update(id):
 
         return redirect(url_for('index'))
     return render_template("update.html", form=form)
+
+
+
+
+
+
+
+@app.route("/delete/<id>")
+def delete(id):
+        id = request.args.get("id")
+        movie = db.get_or_404(Movies, id)
+        db.session.delete(movie)
+        db.session.commit()
+        return redirect(url_for("index"))
+
+
+
+
+
+
+
+
+
+@app.route("/add", methods=["GET","POST"])
+def add():
+
+
+    class MyForm (FlaskForm):
+        movie_title = StringField("Title", validators=[DataRequired()])
+        sub_button = SubmitField("Add", validators=[DataRequired()])
+    form = MyForm()
+    if form.validate_on_submit():
+
+        api_params = {
+            "query" : form["movie_title"].data,
+            "api_key": API_KEY,
+
+        }
+        response = requests.get(API_END_POINT, api_params)
+        response.raise_for_status()
+        data = response.json()
+
+        result = data["results"]
+        return render_template("select.html", result=result)
+
+    return render_template("add.html", form=form)
+
+
+
+
+
+
 
 
 
